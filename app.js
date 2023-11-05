97fea2a25a814a2f9d1361f1aeb04332cd51496c
mudasirabdullah52 <mudasir.ahmad.mca.2019@miet.ac.in>
2023-11-05 17:18:29 +0530

    clean up  code

--- app.js
+++ app.js
@@ -1,56 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./route');
 
-const server = http.createServer((req, res) => {
-    // console.log(req.url, req.headers, req.method);
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-
-        const data = fs.readFileSync('example.txt', 'utf8');
-
-        res.write('<html>');
-        res.write('<head><title>First page</title></head>');
-        res.write(`<body><h4>${data}</h4> <form action="/massage" method ="POST"> <input type = "text" name="massage"> <button type="submit">Send</button></form></body>`);
-        res.write('</html>');
-        return res.end();
-
-
-    }
-    if (url === '/massage' && method === 'POST') {
-
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        })
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const massage = parseBody.split('=')[1];
-            console.log(parseBody);
-            fs.writeFile('example.txt', massage, (err) => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    res.statusCode = 302;
-                    res.setHeader('location', '/');
-                    return res.end();
-
-                }
-            })
-
-        })
-
-
-
-    }
-    // res.setHeader('Content-Type', "text/html")
-    // res.write('<html>');
-    // res.write('<head><title>First page</title></head>');
-    // res.write('<body><h1>Hello from node js</h1></body>');
-    // res.write('</html>');
-    // res.end();
-})
+const server = http.createServer(routes);
 
 server.listen(4000);

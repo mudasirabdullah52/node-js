49b0a3dcb9cd826360b270ba7588442cd854c301
mudasirabdullah52 <mudasir.ahmad.mca.2019@miet.ac.in>
2023-11-12 21:33:26 +0530

    code updated with model

--- models/product.js
+++ models/product.js
@@ -1,22 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
+// path for file system 
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
 
+// Helper function 
+const getProductFromFile = (cb) => {
+    fs.readFile(p, (err, fileCotent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileCotent));
+        }
+
+    });
+
+}
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
-
     save() {
         // products.push(this); // instead of storing in array we will store it in the file
-        const p = path.join(path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-        );
-
+        getProductFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        })
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProductFromFile(cb);
     }
 }
